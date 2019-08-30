aebc059781fb15b111b5f594a1372514a940ec41
Ben <ben@benzeng.co.uk>
2019-08-30 11:15:32 +0100

    complete

--- lib/assertion_list.rb
+++ lib/assertion_list.rb
@@ -9,11 +9,9 @@ class AssertionList
 
   def run_all_assertions
     @assertions.each do |assertion|
-      if assertion.is_a?(TruthyAssertion)
-        assertion.truthy
-      elsif assertion.is_a?(EqualAssertion)
-        assertion.equal
-      end
+        assertion.check
+    end
+
+    @assertions.each(&:check)
     end
   end
-end
